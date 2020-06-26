b5460e9fab61191a9749f7628abaa7314538fcef
Andrzej Sobczyk <nooneeverreallydies@tlen.pl>
2020-06-26 09:20:42 +0200

    changed some classes to functions

--- src/BookCase.js
+++ src/BookCase.js
@@ -1,28 +1,26 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import BookShelf from './BookShelf'
 import { Link } from 'react-router-dom'
 
-class BookCase extends Component {
-
-  render() {
-    return(
-      <div className="list-books">
-        <div className="list-books-title">
-          <h1>MyReads</h1>
-        </div>
-        <div className="list-books-content">
-          <BookShelf books={ this.props.books } shelf={ 'currentlyReading' } shelfName={ 'Currently Reading' } onShelfChange={ this.props.onShelfChange } />
-          <BookShelf books={ this.props.books } shelf={ 'wantToRead' } shelfName={ 'Want To Read' } onShelfChange={ this.props.onShelfChange } />
-          <BookShelf books={ this.props.books } shelf={ 'read' } shelfName={ 'Read' } onShelfChange={ this.props.onShelfChange } />
-        </div>
-        <div className="open-search">
-          <Link className='open-search' to='/search'>Add a book</Link>
-        </div>
+function BookCase(props) {
+  return(
+    <div className="list-books">
+      <div className="list-books-title">
+        <h1>MyReads</h1>
       </div>
-    )
-  }
+      <div className="list-books-content">
+        <BookShelf books={ props.books } shelf={ 'currentlyReading' } shelfName={ 'Currently Reading' } onShelfChange={ props.onShelfChange } />
+        <BookShelf books={ props.books } shelf={ 'wantToRead' } shelfName={ 'Want To Read' } onShelfChange={ props.onShelfChange } />
+        <BookShelf books={ props.books } shelf={ 'read' } shelfName={ 'Read' } onShelfChange={ props.onShelfChange } />
+      </div>
+      <div className="open-search">
+        <Link className='open-search' to='/search'>Add a book</Link>
+      </div>
+    </div>
+  )
 }
+
 
 BookCase.propTypes = {
     books: PropTypes.array.isRequired,
--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,29 +1,26 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import Book from './Book'
 
-class BookShelf extends Component {
-
-  render() {
-    const listBooks = this.props.books.filter((book)=> (
-        book.shelf === this.props.shelf
-    ))
+function BookShelf(props) {
+  const listBooks = props.books.filter((book)=> (
+    book.shelf === props.shelf
+  ))
     
-    return(
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">{ this.props.shelfName }</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-            { listBooks.map((book)=> (
-              <li key={ book.id }>
-                <Book book={ book } onShelfChange={ this.props.onShelfChange } />    
-              </li>
-            )) }
-          </ol>
-        </div>
+  return(
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">{ props.shelfName }</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          { listBooks.map((book)=> (
+            <li key={ book.id }>
+              <Book book={ book } onShelfChange={ props.onShelfChange } />    
+            </li>
+          )) }
+        </ol>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 BookShelf.propTypes = {
